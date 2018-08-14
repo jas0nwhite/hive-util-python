ef5be624a1ddf00dd1714bd373f50725d2169b8b
Jason White <jas0nw@vtc.vt.edu>
2018-08-14 15:02:38 -0400

    added end-to-end test for ABF conversion -- found a possible bug in pyabf?

---
+++ test/functional/test_abf.py
@@ -0,0 +1,13 @@
+'''
+Created on Aug 14, 2018
+
+@author: jwhite
+'''
+
+from pyabf import ABF
+
+if __name__ == '__main__':
+    a = ABF(
+        abf='/Volumes/external/hnl/incoming/PD_surgery_data/2017_08_25/2017_08_25_0004.abf',
+        preLoadData=False,
+        atfStorage=None)
