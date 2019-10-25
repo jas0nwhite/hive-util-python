fb4ffc81621cc967a3b31e049260a52e1d3f88a6
Jason White <jas0nw@vtc.vt.edu>
2019-10-25 14:55:00 -0400

    use signal modes to do a better job of finding TTL threshold

--- src/hive/signal/ttl.py
+++ src/hive/signal/ttl.py
@@ -7,36 +7,49 @@ Created on Oct 16, 2018
 """
 
 from dfply import *  # @UnusedWildImport
+import statistics as stats
 
 
 def find_edges(input_df):
+    # find the low/quiescent (most common) voltage
+    v_lo = min(_modes(input_df.voltage))
 
-    # find the range
-    v_min = input_df.voltage.min()
-    v_max = input_df.voltage.max()
+    # find the most common high/active voltage
+    high_df = (
+            input_df >>
+            mask(X.voltage > (v_lo + 1.0))
+    )
+    v_hi = max(_modes(high_df.voltage))
 
     # set threshold to 2/3
-    threshold = v_min + (v_max - v_min) * 2 / 3
+    threshold = v_lo + (v_hi - v_lo) * 2 / 3
 
     # compute difference of signal
     signal = (
-        input_df >>
-        mutate(hi=1 * (X.voltage > threshold)) >>
-        mutate(dHi=X.hi.diff(), dt=X.time.diff())
+            input_df >>
+            mutate(hi=1 * (X.voltage > threshold)) >>
+            mutate(dHi=X.hi.diff(), dt=X.time.diff())
     )
 
     up = (
-        signal >>
-        mask(X.dHi > 0) >>
-        mutate(onset=X.time - (X.dt / 2)) >>
-        mutate(interval=X.onset.diff())
+            signal >>
+            mask(X.dHi > 0) >>
+            mutate(onset=X.time - (X.dt / 2)) >>
+            mutate(interval=X.onset.diff())
     ).reset_index(drop=True)
 
     dn = (
-        signal >>
-        mask(X.dHi < 0) >>
-        mutate(onset=X.time - (X.dt / 2)) >>
-        mutate(interval=X.onset.diff())
+            signal >>
+            mask(X.dHi < 0) >>
+            mutate(onset=X.time - (X.dt / 2)) >>
+            mutate(interval=X.onset.diff())
     ).reset_index(drop=True)
 
     return up, dn, threshold
+
+
+def _modes(d):
+    # noinspection PyProtectedMember
+    table = stats._counts(d)
+    modes = [table[i][0] for i in range(len(table))]
+    return modes
